2dfea8d6c6a1e6e61253e62a689cf68696321b4b
Dohner, James Thomas <jtdohner@iu.edu>
2023-04-24 02:02:06 -0400

    Queen Completed

--- Game_Board/main/set/games/chess/Renders/rk_ui/general/tokenType.js
+++ Game_Board/main/set/games/chess/Renders/rk_ui/general/tokenType.js
@@ -1163,7 +1163,426 @@ class Queen {
     imgB = "lib/tokenB/queenB.png";
     imgW = "lib/tokenW/queenW.png";
 
-    MoveCheck(x, y, self) { }
+    MoveCheck(x, y, self) {
+        let moves = []
+        let East = this.MoveEast(x, y, self)
+        let West = this.MoveWest(x, y, self)
+        let North = this.MoveNorth(x, y, self)
+        let South = this.MoveSouth(x, y, self)
+        let NorthEast = this.CheckNorthEast(x, y, self)
+        let NorthWest = this.CheckNorthWest(x, y, self)
+        let SouthEast = this.CheckSouthEast(x, y, self)
+        let SouthWest = this.CheckSouthWest(x, y, self)
+
+
+        //Check if moves are avalable for each direction
+        if (East) {
+            East.forEach((tile) => { moves.unshift(tile) })
+        }
+        if (West) {
+            console.log("West")
+            console.log(West)
+            West.forEach((tile) => { moves.unshift(tile) })
+        }
+        if (North) {
+            console.log(North)
+            North.forEach((tile) => { moves.unshift(tile) })
+        }
+        if (South) {
+            console.log(South)
+            South.forEach((tile) => { moves.unshift(tile) })
+        }
+        if (NorthEast) {
+            NorthEast.forEach((tile) => { moves.unshift(tile) })
+
+        }
+        if (NorthWest) {
+            NorthWest.forEach((tile) => { moves.unshift(tile) })
+
+        }
+        if (SouthEast) {
+            SouthEast.forEach((tile) => { moves.unshift(tile) })
+
+        }
+        if (SouthWest) {
+            SouthWest.forEach((tile) => { moves.unshift(tile) })
+
+        }
+
+
+
+        //Check that moves are avalable
+        if (moves.length) {
+            return moves
+        }
+        else {
+            return false
+
+        }
+    }//End Of Move Check
+
+    MoveEast(x, y, self) {
+
+        let moves = []
+
+        for (var newX = x + 1; newX < 8; newX++) {
+            let target = `#x-${newX}_y-${y}`
+            let targetCheck = $(target).children()
+
+            //Tile is Blocked
+            if (targetCheck.length) {
+                let targetName = targetCheck[0].id
+                let targetOwn = targetName[0]
+
+                if (targetOwn != self) {
+                    console.log("Atack")
+                    moves.unshift({ a: "A", tial: target })
+
+                }
+                else {
+                    break
+                }
+
+            }
+
+            //Tile is open
+            else {
+
+                moves.unshift({ a: "O", tial: target })
+            }
+
+        }
+
+        if (moves.length) {
+            return moves
+        }
+        else {
+            return false
+        }
+
+    }
+
+    MoveWest(x, y, self) {
+
+        let moves = []
+
+        for (var newX = x - 1; newX > -1; newX--) {
+            let target = `#x-${newX}_y-${y}`
+            let targetCheck = $(target).children()
+
+            //Tile is Blocked
+            if (targetCheck.length) {
+                let targetName = targetCheck[0].id
+                let targetOwn = targetName[0]
+
+                if (targetOwn != self) {
+                    console.log("Atack")
+                    moves.unshift({ a: "A", tial: target })
+
+                }
+                else {
+                    break
+                }
+
+
+
+            }
+            //Tile is open
+            else {
+
+                moves.unshift({ a: "O", tial: target })
+            }
+
+        }
+
+        if (moves.length) {
+            return moves
+        }
+        else {
+            return false
+        }
+
+    }
+
+    MoveNorth(x, y, self) {
+
+        let moves = []
+
+        for (var newY = y + 1; newY < 8; newY++) {
+            let target = `#x-${x}_y-${newY}`
+            let targetCheck = $(target).children()
+
+            //Tile is Blocked
+            if (targetCheck.length) {
+                let targetName = targetCheck[0].id
+                let targetOwn = targetName[0]
+
+                if (targetOwn != self) {
+                    console.log("Atack")
+                    moves.unshift({ a: "A", tial: target })
+
+                }
+                else {
+                    break
+                }
+
+
+
+            }
+            //Tile is open
+            else {
+
+                moves.unshift({ a: "O", tial: target })
+            }
+
+        }
+
+        if (moves.length) {
+            return moves
+        }
+        else {
+            return false
+        }
+
+    }
+
+    MoveSouth(x, y, self) {
+
+        let moves = []
+
+        for (var newY = y - 1; newY > -1; newY--) {
+            let target = `#x-${x}_y-${newY}`
+            let targetCheck = $(target).children()
+
+            //Tile is Blocked
+            if (targetCheck.length) {
+                let targetName = targetCheck[0].id
+                let targetOwn = targetName[0]
+
+                if (targetOwn != self) {
+                    console.log("Atack")
+                    moves.unshift({ a: "A", tial: target })
+
+                }
+                else {
+                    break
+                }
+
+            }
+
+            //Tile is open
+            else {
+
+                moves.unshift({ a: "O", tial: target })
+            }
+
+        }
+
+        if (moves.length) {
+            return moves
+        }
+        else {
+            return false
+        }
+
+    }
+
+    CheckNorthEast(x, y, self) {
+        let moves = []
+        let newX = x + 1
+        let target
+        let targetCheck
+
+        for (var newY = y + 1; newY < 8; newY++) {
+            //Set Target Tile
+            target = `#x-${newX}_y-${newY}`
+            targetCheck = $(target).children()
+
+            ////Check for Token on target tile
+            if (targetCheck.length) {
+                let targetName = targetCheck[0].id
+                let targetOwn = targetName[0]
+
+                if (targetOwn != self) {
+                    console.log("Atack")
+                    moves.unshift({ a: "A", tial: target })
+                    break
+                }
+                else {
+                    break
+                }
+
+
+
+            }
+            //Tile is open
+            else {
+
+                moves.unshift({ a: "O", tial: target })
+            }
+            newX++
+        }
+
+        if (moves.length) {
+            return moves
+        }
+        else {
+            return false
+        }
+    }
+
+    CheckNorthWest(x, y, self) {
+        let moves = []
+        let newX = x - 1
+        let target
+        let targetCheck
+
+        for (var newY = y + 1; newY < 8; newY++) {
+            //Set Target Tile
+            if (newX > 8 || newX < 0) {
+                break
+            }
+            target = `#x-${newX}_y-${newY}`
+            targetCheck = $(target).children()
+
+            ////Check for Token on target tile
+            if (targetCheck.length) {
+                let targetName = targetCheck[0].id
+                let targetOwn = targetName[0]
+
+                if (targetOwn != self) {
+                    console.log("Atack")
+                    moves.unshift({ a: "A", tial: target })
+                    break
+                }
+                else {
+                    break
+                }
+
+
+
+            }
+            //Tile is open
+            else {
+
+                moves.unshift({ a: "O", tial: target })
+            }
+            newX--
+        }
+
+        if (moves.length) {
+            return moves
+        }
+        else {
+            return false
+        }
+    }
+
+    CheckSouthEast(x, y, self) {
+        let moves = []
+        let newX = x + 1
+        let target
+        let targetCheck
+
+        for (var newY = y - 1; newY > -1; newY--) {
+            //Set Target Tile
+            if (newX != -1 || newX != 8) {
+                target = `#x-${newX}_y-${newY}`
+                targetCheck = $(target).children()
+            }
+            else {
+                break
+            }
+
+
+            ////Check for Token on target tile
+            if (targetCheck.length) {
+                let targetName = targetCheck[0].id
+                let targetOwn = targetName[0]
+
+                if (targetOwn != self) {
+                    console.log("Atack")
+                    moves.unshift({ a: "A", tial: target })
+                    break
+                }
+                else {
+                    break
+                }
+
+
+
+            }
+            //Tile is open
+            else {
+
+                moves.unshift({ a: "O", tial: target })
+            }
+            newX++
+        }
+
+        if (moves.length) {
+            return moves
+        }
+        else {
+            return false
+        }
+    }
+
+    CheckSouthWest(x, y, self) {
+        let moves = []
+        let newX = x - 1
+        let target
+        let targetCheck
+
+        for (var newY = y - 1; newY > -1; newY--) {
+            //Set Target Tile
+            if (newX > 8 || newX < 0) {
+                break
+            }
+            target = `#x-${newX}_y-${newY}`
+            targetCheck = $(target).children()
+
+            ////Check for Token on target tile
+            if (targetCheck.length) {
+                if (newX != -1 || newX != 8) {
+                    let targetName = targetCheck[0].id
+                    let targetOwn = targetName[0]
+
+                    if (targetOwn != self) {
+                        console.log("Atack")
+                        moves.unshift({ a: "A", tial: target })
+                        break
+                    }
+                    else {
+                        break
+                    }
+                }
+                else {
+                    break
+                }
+
+
+
+
+
+
+            }
+            //Tile is open
+            else {
+
+                moves.unshift({ a: "O", tial: target })
+            }
+            newX--
+        }
+
+        if (moves.length) {
+            return moves
+        }
+        else {
+            return false
+        }
+
+    }
 
 }//End Of Queen
 
